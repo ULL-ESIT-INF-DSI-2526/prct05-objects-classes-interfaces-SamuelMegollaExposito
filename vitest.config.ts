ea0e7003be15cf2132749333a4d304cb794ef9ae
Samuel Megolla <alu0101567198@ull.edu.es>
2026-03-03 20:04:11 +0000

    Coveralls coverage info and Github Actions

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['tests/**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.ts']
+    }
+  }
+})
